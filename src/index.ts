5ad7086f0281f1da66d4a0a778aaebc2cb5b59ab
trepichio <trepichio@gmail.com>
2021-03-23 05:57:46 -0300

    implement gameloop and start game to draw bricks

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,9 @@ import {
   BALL_STARTY,
 } from "./setup";
 
+// Helpers
+import { createBricks } from './helpers';
+
 let gameOver = false;
 let score = 0;
 
@@ -34,12 +37,27 @@ function setGameWin(view: CanvasView) {
 
 function gameLoop(
   view: CanvasView,
-  bricks: Brick[],
-  paddle: Paddle,
-  ball: Ball
-) {}
+  bricks: Brick[]
+  // paddle: Paddle,
+  // ball: Ball
+) {
+  view.clear();
+  view.drawBricks(bricks);
 
-function startGame(view: CanvasView) {}
+  requestAnimationFrame(() => gameLoop(view, bricks));
+}
+
+function startGame(view: CanvasView) {
+  // Reset displays
+  score = 0;
+  view.drawInfo("");
+  view.drawScore(0);
+
+  // Create all bricks
+  const bricks = createBricks();
+
+  gameLoop(view, bricks);
+}
 
 // Create a new view
 const view = new CanvasView("#playField");
