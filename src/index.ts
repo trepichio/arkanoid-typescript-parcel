509b32bb6bee24155212da300f48ce929224a45b
trepichio <trepichio@gmail.com>
2021-03-24 05:00:04 -0300

    add GameOver and GameWin

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,13 @@ function gameLoop(
     view.drawScore(score);
   }
 
+  // Game Over when ball leaves playfield
+  if (ball.pos.y > view.canvas.height) gameOver = true;
+  // If game won, set gameOver and display win
+  if (bricks.length === 0) return setGameWin(view);
+  // Return if gameover and don't run the requestAnimationFrame
+  if (gameOver) return setGameOver(view);
+
   requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball, collision));
 }
 
